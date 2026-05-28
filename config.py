38d94e94130d9c1a1d5d24a7dbf6e02ee4973da8
jorige3 <jorige369@gmail.com>
2026-05-28 16:41:05 +0530

    add file type configuration

---
+++ config.py
@@ -0,0 +1,8 @@
+FILE_TYPES = {
+"Images": [".jpg", ".jpeg", ".png", ".gif"],
+"Documents": [".pdf", ".docx", ".txt", ".xlsx"],
+"Videos": [".mp4", ".mkv", ".avi"],
+"Audio": [".mp3", ".wav"],
+"Archives": [".zip", ".rar"],
+"Code": [".py", ".js", ".html", ".css"],
+}
